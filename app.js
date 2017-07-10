d29c3319f47c1b5e86e56a44fdf91d8936b9b215
Loureiro Daniel <daniel.loureiro@ecl16.ec-lyon.fr>
2017-07-10 23:21:38 +0200

    Mise a jour du serveur suite aux modifs precedentes

--- app.js
+++ app.js
@@ -1,28 +1,20 @@
 // var http = require('http');
 var express = require('express');
-var session = require('express-session');
-var CasAuth = require('cas-authentication');
 var app = express();
-// var servStatic = require('./servStatic').servStaticFile;
+var session = require('express-session');
+// var mongoose=require("mongoose");
+// var User = require("./models/user");
+var authentify = require("./primary/authentification.js")
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public')); // Definition du repertoire statique
 
-// ------Parametrage du CAS---------
+// ------Initialization de la connection--------
 app.use(session({
     secret      : 'dats a fuckin good secret',
     resave      : false,
     saveUninitialized : true
 }));
-
-var cas = new CasAuth({
-    cas_url         : 'https://cas.ec-lyon.fr/cas',
-    service_url     : 'http://localhost:8080',
-    cas_version     : '2.0'
-});
-
-app.use(cas.bounce)
-
-//-----------------------------------
+//---------------------------------------------
 
 
 
@@ -30,7 +22,7 @@ app.get("/",function(req, res){
     res.render("welcome.ejs",{user:req.session.cas_user});
 });
 
-app.get("/private",cas.block,function(req,res){
+app.get("/private",authentify,function(req,res){
     res.send("You accessed the private content!");
 })
 
