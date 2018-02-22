724636200a5afbf15c1d67bce0c56111db785c29
tcavigna <theo.cavignac@ecl16.ec-lyon.fr>
2018-02-22 16:54:16 +0100

    Ajout d'un groupe admin et d'un alias

--- config.sh
+++ config.sh
@@ -141,12 +141,13 @@ cat <<EOF | sed "s?@URL?$URL?g" | sed "s?@LURL?$LURL?" | sed "s?@ROOT_PATH?$ROOT
                 "name" : "VARCHAR(127) UNIQUE",
                 "description" : "TEXT"
             },
-            "init" : "REPLACE INTO user_group (id, name, description) VALUES (0, \"ecl\", \"Centraliens de Lyon\");"
+            "init" : "REPLACE INTO user_group (id, name, description) VALUES (0, \"ecl\", \"Centraliens de Lyon\"); REPLACE INTO user_group (id, name, description) VALUES (1, \"admin\", \"Administrateurs du site\");"
         }
     ],
 
     "alias" : {
-        "#ecl" : "SELECT login FROM user JOIN membership ON user.id = membership.id_user WHERE membership.id_group = 0;"
+        "#ecl" : "SELECT login FROM user JOIN membership ON user.id = membership.id_user WHERE membership.id_group = 0;",
+        "#admin" : "SELECT login FROM user JOIN membership ON user.id = membership.id_user WHERE membership.id_group = 1;"
     },
 
     "cas_config" : {
