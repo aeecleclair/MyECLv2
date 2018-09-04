dbf4d641fd7e0ebc999ef96360902f7fa475a565
Théo Cavignac (Lattay) <theo.cavignac@ecl16.ec-lyon.fr>
2018-09-04 18:38:33 +0200

    Interdit les doublons de login à l'inscription

--- primary/authenticate.js
+++ primary/authenticate.js
@@ -77,7 +77,6 @@ module.exports = function(context){
 
     exports.create_account = function(req, res){
         // doit être utilisé avec POST
-        // TODO eviter les doublons
         // TODO Tester la validité des informations fournies ?
 
         context.csrf.checkToken(req.body.login, req.body['__token'], 'new_account', function(err, valid){
@@ -94,75 +93,83 @@ module.exports = function(context){
                 if(req.body.login && req.body.password){
                     context.crypto.hash(req.body.password, function(err, hash){
                         if(!err){
-                            var user = new Object();
-
-                            // validation
-                            if(
-                                !context.validator.isAlpha(req.body.login) ||
-                                req.body.login.length > 16 || // longueur fixé dans la table user
-                                !context.validator.matches(req.body.promo, '([0-9]+[Ee]?)?') ||
-                                !context.validator.matches(req.body.floor, '([A-Z][0-9]+)?') ||
-                                !context.validator.isNumeric(req.body.picselector)
-                            ){
+                            if(!context.validator.isAlpha(req.body.login)){
                                 return res.redirect('/login.html?invalid=1');
                             }
-                            // sanitization
-                            context.validator.escape(req.body.name);
-                            context.validator.escape(req.body.firstname);
-                            context.validator.escape(req.body.nick);
-
-                            user['login'] = req.body.login;
-                            user['password'] = hash;
-                            user['name'] = req.body.name;
-                            user['firstname'] = req.body.firstname;
-                            user['nick'] = req.body.nick;
-                            // user['birth'] = req.body.birth; // TODO à formater en date YYYY-MM-DD
-                            user['gender'] = req.body.gender == 'Femme' ? 'F' : 'H';
-                            user['promo'] = req.body.promo;
-                            user['floor'] = req.body.floor;
-
-                            if(req.body.picselector == '3'){
-                                user['picture_path'] = req.file.path;
-                                user['picture_url'] = '/user_upload/' + req.file.filename;
-                            } else {
-                                // TODO ameliorer le choix les photos par defaut
-                                user['picture_path'] = '';
-                                user['picture_url'] = '/picture/default_pic_' + req.body.picselector + '.png';
-                            }
-
-                            // user['email'] = req.session.user_data.email; // TODO verifier que req.session.user_data.email existe
-                            context.database.save('user', user, function(err){
-                                if(!err){
-                                    req.session.user = user;
-                                    res.redirect('/home');
-                                    context.database.select('user', ['id'], 'login = ?', [user.login], function(err2, res){
-                                        if(!err2 && res[0]){
-                                            context.database.save('membership',
-                                                {
-                                                    'id_user' : res[0]['id'],
-                                                    'id_group' : 2, // ecl
-                                                    'position' : 'élève', // TODO améliorer avec les infos du CAS
-                                                    'term' : ''
-                                                },
-                                                function(err3){
-                                                    if(err3){
-                                                        context.log.error('Unable to set ' + user.login + ' ecl membership :');
-                                                        context.log.error(err3);
-                                                    }
-                                                }
-                                            );
-                                        } else {
-                                            context.log.error('Unable to get ' + user.login + ' id :');
-                                            context.log.error(err2);
-                                        }
-                                    });
-                                } else {
-                                    context.log.error('Unable to save user');
-                                    context.log.info(user);
-                                    context.log.error(err);
-                                    res.status(500);
-                                    res.send('<meta http-equiv="refresh" content="5; URL=/login.html"> DB Fail');
+                            get_user(req.body.login, function(err, rows){
+                                if(err || rows.length != 0){
+                                    return res.redirect('/login.html?duplicate=1');
                                 }
+
+                                var user = new Object();
+
+                                // validation
+                                if(
+                                    req.body.login.length > 16 || // longueur fixé dans la table user
+                                    !context.validator.matches(req.body.promo, '([0-9]+[Ee]?)?') ||
+                                    !context.validator.matches(req.body.floor, '([A-Z][0-9]+)?') ||
+                                    !context.validator.isNumeric(req.body.picselector)
+                                ){
+                                    return res.redirect('/login.html?invalid=1');
+                                }
+                                // sanitization
+                                context.validator.escape(req.body.name);
+                                context.validator.escape(req.body.firstname);
+                                context.validator.escape(req.body.nick);
+
+                                user['login'] = req.body.login;
+                                user['password'] = hash;
+                                user['name'] = req.body.name;
+                                user['firstname'] = req.body.firstname;
+                                user['nick'] = req.body.nick;
+                                // user['birth'] = req.body.birth; // TODO à formater en date YYYY-MM-DD
+                                user['gender'] = req.body.gender == 'Femme' ? 'F' : 'H';
+                                user['promo'] = req.body.promo;
+                                user['floor'] = req.body.floor;
+
+                                if(req.body.picselector == '3'){
+                                    user['picture_path'] = req.file.path;
+                                    user['picture_url'] = '/user_upload/' + req.file.filename;
+                                } else {
+                                    // TODO ameliorer le choix les photos par defaut
+                                    user['picture_path'] = '';
+                                    user['picture_url'] = '/picture/default_pic_' + req.body.picselector + '.png';
+                                }
+
+                                // user['email'] = req.session.user_data.email; // TODO verifier que req.session.user_data.email existe
+                                context.database.save('user', user, function(err){
+                                    if(!err){
+                                        req.session.user = user;
+                                        res.redirect('/home');
+                                        context.database.select('user', ['id'], 'login = ?', [user.login], function(err2, res){
+                                            if(!err2 && res[0]){
+                                                context.database.save('membership',
+                                                    {
+                                                        'id_user' : res[0]['id'],
+                                                        'id_group' : 2, // ecl
+                                                        'position' : 'élève', // TODO améliorer avec les infos du CAS
+                                                        'term' : ''
+                                                    },
+                                                    function(err3){
+                                                        if(err3){
+                                                            context.log.error('Unable to set ' + user.login + ' ecl membership :');
+                                                            context.log.error(err3);
+                                                        }
+                                                    }
+                                                );
+                                            } else {
+                                                context.log.error('Unable to get ' + user.login + ' id :');
+                                                context.log.error(err2);
+                                            }
+                                        });
+                                    } else {
+                                        context.log.error('Unable to save user');
+                                        context.log.info(user);
+                                        context.log.error(err);
+                                        res.status(500);
+                                        res.send('<meta http-equiv="refresh" content="5; URL=/login.html"> DB Fail');
+                                    }
+                                });
                             });
                         } else {
                             context.log.error('Unable to hash password');
@@ -170,20 +177,20 @@ module.exports = function(context){
                             res.status(500);
                             res.send('<meta http-equiv="refresh" content="5; URL=/login.html"> Hash Fail');
                         }
+                        /*
+                            "id" : "INT PRIMARY KEY NOT NULL",
+                            "login" : "VARCHAR(255)",
+                            "password" : "VARCHAR(255)",
+                            "name" : "VARCHAR(255)",
+                            "firstname" : "VARCHAR(255)",
+                            "nick" : "VARCHAR(255)",
+                            "birth" : "DATE",
+                            "gender" : "VARCHAR(1)",
+                            "promo" : "INT",
+                            "floor" : "VARCHAR(3)",
+                            "groups" : "TEXT"
+                        */
                     });
-                    /*
-                        "id" : "INT PRIMARY KEY NOT NULL",
-                        "login" : "VARCHAR(255)",
-                        "password" : "VARCHAR(255)",
-                        "name" : "VARCHAR(255)",
-                        "firstname" : "VARCHAR(255)",
-                        "nick" : "VARCHAR(255)",
-                        "birth" : "DATE",
-                        "gender" : "VARCHAR(1)",
-                        "promo" : "INT",
-                        "floor" : "VARCHAR(3)",
-                        "groups" : "TEXT"
-                    */
                 } else {
                     res.redirect('/login.html?invalid=1');
                 }
